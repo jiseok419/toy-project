c0429b8f3c5ae1273f71fc80a34ba43bb1e4d5f2
jiseok419 <pyosik0419@gmail.com>
2023-03-08 12:28:45 +0900

    :recycle: :: 파일 이름 변경

--- openai/src/main/java/com/study/openai/global/config/WebMvcConfig.java
+++ openai/src/main/java/com/study/openai/global/config/WebMvcConfig.java
@@ -3,7 +3,7 @@ package com.study.openai.global.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class MvcWebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**");
